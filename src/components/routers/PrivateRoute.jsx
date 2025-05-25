318b3e1e99394335b8b2d021590490816aa4e228
EmersonOkemoto <135055470+EmersonOkemoto@users.noreply.github.com>
2025-05-25 15:09:12 -0300

    PrivateRoute e AuthContext

--- src/components/routers/PrivateRoute.jsx
+++ src/components/routers/PrivateRoute.jsx
@@ -1,14 +1,25 @@
-//src/components/routers/PrivateRoute.jsx
+// src/components/routers/PrivateRoute.jsx
 
-import React from "react";
+/*Aula anotacao - Componente de Rota Privada
+    Componente para verificar se o usuário está autenticado.
+    Se não estiver autenticado, redirecionar para rota pública (página inicial).
+    Se estiver autenticado, pode acessar a rota.
+*/
+
+import { useContext } from "react";
+import { AuthContext } from "../context/auth"; // Importa o contexto de autenticação
 import { Navigate } from "react-router-dom";
 
-// Verifica se o usuário está logado
-const isAuthenticated = () => {
-  // verifica se tem um token no localStorage
-  return localStorage.getItem("token") !== null;
-};
-
 export default function PrivateRoute({ children }) {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+  // Acessa o valor do estado de autenticação (auth) a partir do contexto
+  const { auth } = useContext(AuthContext);
+
+  // Se o usuário NÃO estiver autenticado (auth === false) ou (!auth)
+  // redirecionar para uma rota pública, como a página inicial ("/") 
+  if (auth === false) {
+    return <Navigate to="/" />;
+  }
+
+  // Se estiver autenticado, renderiza os componentes filhos (rota protegida)
+  return children;
 }
