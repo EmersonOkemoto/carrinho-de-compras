dac4f8c256e7234dbb90705ae2eebcaf4ecb70fd
EmersonOkemoto <135055470+EmersonOkemoto@users.noreply.github.com>
2025-05-25 23:19:15 -0300

    Update main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import AuthProvider from './components/context/auth';
+import DataProvider from './components/context/data';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
